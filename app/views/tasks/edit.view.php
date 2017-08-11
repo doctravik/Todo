bad6d8683377035854b2cf858f4cf3ff1e2be3c1
doctravik <doctravik@gmail.com>
2017-08-11 03:22:33 +0300

    Update edit page

--- app/views/tasks/edit.view.php
+++ app/views/tasks/edit.view.php
@@ -1,19 +1,29 @@
 <?php $layout = 'app/views/layouts/app.view.php' ?>
 
+<div class="row">
+    <div class="col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">Edit task</h3>
+            </div>
+            <div class="panel-body">
+                <form action=<?="/tasks/{$task->id}/update";?> method="POST">
+                    <div class="form-group <?= errors('content')->exists() ? 'has-error' : '' ;?>">
+                        <label for="content" class="control-label">Content</label>
+                        <textarea id="content" name="content" class="form-control" rows="5"><?=
+                            htmlspecialchars(old('content', $task->content)) ; 
+                        ?></textarea>
 
-<form action=<?="/tasks/{$task->id}/update";?> method="POST">
-    <div class="form-group <?= errors('content')->exists() ? 'has-error' : '' ;?>">
-        <label for="content" class="control-label">Content</label>
-        <textarea id="content" name="content" class="form-control" rows="5"><?=
-            htmlspecialchars(old('content', $task->content)) ; 
-        ?></textarea>
+                        <?php if(errors('content')->exists()) : ?>
+                            <span class="help-block">
+                                <strong><?= errors('content')->first() ; ?></strong>
+                            </span>
+                        <?php endif ; ?>
+                    </div>
 
-        <?php if(errors('content')->exists()) : ?>
-            <span class="help-block">
-                <strong><?= errors('content')->first() ; ?></strong>
-            </span>
-        <?php endif ; ?>
+                    <button type="submit" class="btn btn-primary">Update</button>
+                </form>
+            </div>
+        </div>
     </div>
-
-    <button type="submit" class="btn btn-primary">Update</button>
-</form>
+</div>
